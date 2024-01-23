d043e12fdb76397e9f87aa83932168ede374c068
Pedro <pedro.phsr2002@gmail.com>
2024-01-22 23:44:00 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite três números");
+
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+double resultado = Maior(n1,n2,n3);
+
+Console.WriteLine("Maior = " +resultado);
+
+static int Maior (int a, int b, int c){
+    int m;
+    if(a > b && a > c){
+        m = a;
+    }
+    else if(b > c){
+        m = b;
+    }
+    else {
+        m = c;
+    }
+    return m;
+}
